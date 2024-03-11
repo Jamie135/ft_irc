5e7e47e9438a6db8bf55573ba3c5307c0886fdff
unknown <jamie.gworld@hotmail.com>
2024-03-12 00:44:51 +0700

    OK

--- main.cpp
+++ main.cpp
@@ -4,3 +4,14 @@
 #include <stdio.h>
 #include <iomanip>
 #include <cstdlib>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		std::cerr << "Error: Syntax must be ./ircserv <port> <password>";
+		return (0);
+	}
+}
